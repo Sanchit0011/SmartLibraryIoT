9389ae9af89b31c42c506564a2b6fc4fbd279337
sanchit <sanchitsh0211@gmail.com>
2019-04-23 18:45:19 +1000

    Changed class name

---
+++ userlogin.py
@@ -0,0 +1,13 @@
+# Imported the necessary modules
+import sys
+import sqlite3
+import os
+import re
+import socket
+import json
+from login_client import login
+from server import server
+
+
+# Defined the userlogin class
+class userlogin:
